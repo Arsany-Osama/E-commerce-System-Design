2016354113a5b8d2ae2200a5a965d9bc3314f8df
Arsany-Osama <arsanyosama3@gmail.com>
2025-07-04 16:00:28 +0300

    output in comment at the end of the code

--- main.java
+++ main.java
@@ -285,3 +285,21 @@ public class main {
         CheckoutService.checkout(customer, cart);
     }
 }
+/*
+Console Output:
+
+** Shipment notice **
+2x Cheese 400g
+1x Biscuits 700g
+Total package weight 1.1kg
+>> Checkout receipt <<
+2x Cheese 200
+1x Biscuits 150
+1x ScratchCard 50
+----------------------
+Subtotal 400
+Shipping 30
+Amount 430
+Customer balance 570
+PS C:\Users\Win10\Desktop\E-commerce> 
+*/
